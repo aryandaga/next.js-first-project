e2696a1965d378dc6cec14d9d6adff0852f26b17
Aryan Daga <aryandg13@gmail.com>
2024-06-16 16:15:44 +0530

    http-equiv to httpEquiv

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ export default function IndexPage() {
       <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
         <div className="inline-block max-w-lg text-center justify-center">
           <title>Redirecting to Another page in HTML</title>
-          <meta http-equiv="refresh" content="2; url ='firebase'" />
+          <meta httpEquiv="refresh" content="2; url ='firebase'" />
 
           <h1 className={title()}>Make&nbsp;</h1>
           <h1 className={title({ color: "violet" })}>beautiful&nbsp;</h1>
